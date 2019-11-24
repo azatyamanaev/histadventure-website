d08a8eace91fd6e8c90a882000ecc2427c3fbb31
monitor <azatyamanaev@yandex.ru>
2019-11-24 20:37:42 +0300

    add method findOneByName in class EventsRepository

--- src/main/java/ru/itis/repositories/EventsRepository.java
+++ src/main/java/ru/itis/repositories/EventsRepository.java
@@ -2,5 +2,8 @@ package ru.itis.repositories;
 
 import ru.itis.entities.Event;
 
+import java.util.Optional;
+
 public interface EventsRepository extends CrudRepository<Event, Long> {
+    Optional<Event> findOneByName(String name);
 }
